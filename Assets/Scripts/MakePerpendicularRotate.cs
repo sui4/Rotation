4e292deb34b29c542e73fe5b7349b8c3a17b7388
ryu sasaki <ryu.sasaki04@gmail.com>
2020-08-12 06:20:35 +0900

    回転軸に垂直な線を二本表示。軸に沿ってどれだけ回転したかを見えるようにした。

---
+++ Assets/Scripts/MakePerpendicularRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePerpendicularRotate : MonoBehaviour
+{
+    public Transform vecInitial;
+    public GameObject vecTerminal;
+    
+    private float rotateAngle;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.position = vecInitial.position;
+        //Vector3 dirction = vecTerminal.transform.position - vecInitial.transform.position;
+        Vector3 direction = vecTerminal.transform.position;
+        Debug.Log(direction);
+        this.transform.up = direction;
+
+        rotateAngle = vecTerminal.GetComponent<Rotate>().rotateAngle;
+        this.transform.rotation = Quaternion.AngleAxis(rotateAngle, transform.up) * this.transform.rotation;
+        //Debug.Log(transform.up);
+    }
+}
