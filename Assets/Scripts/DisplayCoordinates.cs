12dd6bd92abd1bbc6f8d45e38e044b8d5bbbbda3
ryu sasaki <ryu.sasaki04@gmail.com>
2020-08-12 00:56:39 +0900

    XYZ軸と、自由に座標を変更できる回転軸の設定。インスペクタ上から回転角度も変更できるようにした。

---
+++ Assets/Scripts/DisplayCoordinates.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCoordinates : MonoBehaviour
+{
+    public GameObject target;
+    
+    private Vector3 targetPos;
+    private float rotateAngle;
+    private Text text;
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        targetPos = target.transform.position;
+        rotateAngle = target.GetComponent<Rotate>().rotateAngle;
+
+        text.text = "(" + targetPos.x.ToString() +
+                    ", " + targetPos.y.ToString() +
+                    ", " + targetPos.z.ToString() +
+                    ", " + rotateAngle + ")";
+
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject rotateTarget;
+    public float rotateAngle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateAngle = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateTarget.transform.rotation = Quaternion.AngleAxis(rotateAngle, this.transform.position);
+    }
+}
