733d0b590d68480616616eb81f53066d71ddbd81
ryu sasaki <ryu.sasaki04@gmail.com>
2020-09-24 23:18:50 +0900

    quaternionでの回転をVR上で操作できるようにした。 sliderで回転角を設定できるようにした。 回転軸のvectorのtargetオブジェクトについて。 - 座標が色に対応するようにしたが、worldspaceになっているので修正が必要。 - 単位球にくっつくようにした。raycastでsphereとの衝突点に配置される。

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rotate : MonoBehaviour
 {
     public GameObject rotateTarget;
     public float rotateAngle;
+    public GameObject SliderObj;
+    private Slider slider;
     
     // Start is called before the first frame update
     void Start()
     {
         rotateAngle = 0;
+        slider = SliderObj.GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rotateTarget.transform.rotation = Quaternion.AngleAxis(rotateAngle, this.transform.position);
+        rotateAngle = slider.value;
+        rotateTarget.transform.rotation = Quaternion.AngleAxis(rotateAngle, this.transform.position.normalized);
     }
 }
---
+++ Assets/Scripts/VectorController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorController : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    private Color defaultColor;
+    private Color defaultColor_Transparent;
+
+    public bool ray = false;
+    private bool trigStay = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        defaultColor = new Color(1, 1, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!trigStay && ray)
+        {
+            meshRenderer.material.SetColor("_BaseColor", defaultColor);
+
+        }
+        else
+        {
+            meshRenderer.material.SetColor("_BaseColor", Color.red);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        trigStay = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        trigStay = false;
+    }
+}
