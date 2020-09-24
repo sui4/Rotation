904c0136424d4624b22530a9f57ffc292c2fd0ed
ryu sasaki <ryu.sasaki04@gmail.com>
2020-09-25 04:34:14 +0900

    quaternionモデルのbunnyの色は、ベクトルの色に対応させることにした。

---
+++ Assets/Scripts/QuatModelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuatModelController : MonoBehaviour
+{
+    public GameObject vec;
+    public Renderer renderer;
+    private MaterialPropertyBlock materialPropertyBlock;
+
+    private Vector3 color;
+    public Vector4 quatLocalPos;
+    private int quatLocalPosID;
+    // Start is called before the first frame update
+    void Start()
+    {
+                quatLocalPosID = Shader.PropertyToID("_QuatLocalPos");
+        materialPropertyBlock = new MaterialPropertyBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        color = vec.transform.localPosition;
+
+        quatLocalPos = new Vector4(color.x, color.y, color.z, 1.0f);
+        materialPropertyBlock.SetVector(quatLocalPosID, quatLocalPos);
+        renderer.SetPropertyBlock(materialPropertyBlock);
+
+    }
+}
